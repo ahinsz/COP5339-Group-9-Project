7e25203e707835c8f001806dbe7029c0eb7e5959
WinReactor <WinReactor@WinReactor-PC>
2014-11-21 23:46:31 -0500

    Added new keyevent to login. Press enter after entering password

--- src/PopUps/Login.java
+++ src/PopUps/Login.java
@@ -9,12 +9,14 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
 import project.MasterClass;
 
 /**
@@ -22,23 +24,24 @@ import project.MasterClass;
  * @author Andrew
  */
 public class Login {
-    public Login(){
-        
-    }
-    
-    public void openLogin(MasterClass master){
-        JFrame frame = new JFrame("Login");
-	frame.setSize(300, 150);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public Login() {
 
-	JPanel panel = new JPanel();
-	frame.add(panel);
-	placeComponents(panel, master, frame);
+	}
 
-	frame.setVisible(true);
-    }
-    
-    private static void placeComponents(final JPanel panel, final MasterClass master, final JFrame frame) {
+	public void openLogin(MasterClass master) {
+		JFrame frame = new JFrame("Login");
+		frame.setSize(300, 150);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		placeComponents(panel, master, frame);
+
+		frame.setVisible(true);
+	}
+
+	private static void placeComponents(final JPanel panel,
+			final MasterClass master, final JFrame frame) {
 
 		panel.setLayout(null);
 
@@ -60,43 +63,76 @@ public class Login {
 
 		JButton loginButton = new JButton("login");
 		loginButton.setBounds(10, 80, 80, 25);
-                
-                loginButton.addActionListener(new ActionListener()
-                    {
-                       @Override
-                       public void actionPerformed(ActionEvent event)
-                       {
-                           try {
-                               if(master.LoginUser(userText.getText(), new String(passwordText.getPassword()))){
-                                   frame.dispose();
-                                   if(master.isSeller())
-                                       master.openSellerList();
-                                   else
-                                       master.openProductList();
-                               }
-                           } catch (IOException ex) {
-                               Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-                           }
-                           
-                       }
-                    });
-                
+
+		loginButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				try {
+					if (master.LoginUser(userText.getText(), new String(
+							passwordText.getPassword()))) {
+						frame.dispose();
+						if (master.isSeller())
+							master.openSellerList();
+						else
+							master.openProductList();
+					}
+				} catch (IOException ex) {
+					Logger.getLogger(Login.class.getName()).log(Level.SEVERE,
+							null, ex);
+				}
+			}
+
+		});
+
+		passwordText.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					try {
+						if (master.LoginUser(userText.getText(), new String(
+								passwordText.getPassword()))) {
+							frame.dispose();
+							if (master.isSeller())
+								master.openSellerList();
+							else
+								master.openProductList();
+						}
+					} catch (IOException ex) {
+						Logger.getLogger(Login.class.getName()).log(
+								Level.SEVERE, null, ex);
+					}
+				}
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 		panel.add(loginButton);
-		
+
 		JButton registerButton = new JButton("register");
 		registerButton.setBounds(180, 80, 80, 25);
-                
-                registerButton.addActionListener(new ActionListener()
-                    {
-                       @Override
-                       public void actionPerformed(ActionEvent event)
-                       {
-                           frame.dispose();
-                           master.openRegistrationPopup();
-                       }
-                    });
-                
+
+		registerButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				frame.dispose();
+				master.openRegistrationPopup();
+			}
+		});
+
 		panel.add(registerButton);
 	}
-    
+
 }
